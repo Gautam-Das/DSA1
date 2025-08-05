1aeb9fd399071284a41fa5dd2a70c36701d5f5bb
Gautam Das <gdas2005gdas@gmail.com>
2025-08-05 14:08:05 +0930

    Added private variables

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,16 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface Calculator extends Remote {
+
+    ArrayList<Integer> stack = new ArrayList<Integer>();
+    HashMap<String, Runnable> pushOperations = new HashMap<String, Runnable>();
+
     void pushValue(int val) throws RemoteException;
     void pushOperation(String operation) throws RemoteException;
     int pop() throws RemoteException;
     boolean isEmpty() throws RemoteException;
-    int delayPop(int millis) throws RemoteException;
+    int delayPop(int millis) throws RemoteException, InterruptedException;
 }
