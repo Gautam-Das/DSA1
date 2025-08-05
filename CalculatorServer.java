1d14c533aefe1038da42d8429ee34fec655247cb
Gautam Das <gdas2005gdas@gmail.com>
2025-08-05 14:18:24 +0930

    Setup server

---
+++ CalculatorServer.java
@@ -0,0 +1,21 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class CalculatorServer {
+    static String ServerName = "Calculator Service";
+
+    public static void main(String[] args){
+        try {
+            Calculator server = new CalculatorImplementation();
+            Calculator stub = (Calculator) UnicastRemoteObject
+                    .exportObject(server, 0);
+
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind(ServerName, stub);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
